9b738b81b010dc2ebd966d8dc4303b6cbce3ab31
Rodrigo Santos <pedro.vasconcellos.estudante@eteot.rio.br>
2021-11-16 16:56:33 -0300

    atualização

---
+++ script.js/script.js
@@ -0,0 +1,28 @@
+function onScroll()
+   if($(window).scrollTop()<250){
+         $('.scroll').css('display','none');
+   }else{
+ $('.scroll').css('display','block');
+   }
+}
+window.addEventListener('scroll',onScroll,false);
+
+jQuery(document).ready(function($){
+$('.link').click(function(event){
+event.preventDefault();
+$('html,body').animate({scrollTop:})
+
+}
+
+
+
+}
+
+
+
+
+}
+
+
+
+}
